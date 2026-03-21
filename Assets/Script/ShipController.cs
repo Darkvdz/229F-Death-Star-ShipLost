d6920a9f5dc9ff21375fd080d648361040835724
Darkvdz <famparintorn@gmail.com>
2026-03-22 00:11:57 +0700

    Script_ShipController

---
+++ Assets/Script/ShipController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShipController : MonoBehaviour
+{
+    private Rigidbody rb;
+    InputAction moveAction;
+    [Header("Engine Settings")]
+    [SerializeField] private float engineForce;
+    [SerializeField] private float engineTorque;
+    
+    [Header("Drag Setting")]
+    // สูตร Fd = 1/2 * rho * v^2 * Cd * A
+    [SerializeField] private float rho = 1000f;     // p (rho) ความหนาแน่นของน้ำ
+    [SerializeField] private float cd = 0.04f;  // Cd สัมประสิทธิ์แรงต้านของรูปทรงเรือ
+    [SerializeField] private float area = 2.5f;     // A พื้นที่หน้าตัดของเรือ (ตารางเมตร)
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        
+    }
+    
+    void FixedUpdate()
+    {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        float forwardInput = moveInput.y;
+        float turnInput = moveInput.x;
+        rb.AddRelativeForce(engineForce * forwardInput * -Vector3.forward);
+        rb.AddRelativeTorque(engineTorque * turnInput * Vector3.up);
+        
+        float currentSpeed = rb.linearVelocity.magnitude;
+        
+        if (currentSpeed > 0.1f)
+        {
+            float speedSqared = currentSpeed * currentSpeed;
+            Vector3 dragDirection = -rb.linearVelocity.normalized;
+        
+            float dragCalculation = 0.5f * rho * speedSqared * cd * area;
+            Vector3 DragForce = dragDirection * dragCalculation;
+
+            rb.AddForce(DragForce);
+        }
+    }
+}
