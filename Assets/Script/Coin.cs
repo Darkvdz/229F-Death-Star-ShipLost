f721b459a0807eca47fcbdc09c1f4208f4f47f99
Darkvdz <famparintorn@gmail.com>
2026-03-22 03:23:37 +0700

    UI_score

--- Assets/Script/Coin.cs
+++ Assets/Script/Coin.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinSfx;
+    private ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = GameObject.Find("Canvas").GetComponent<ScoreManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-        }
+        AudioSource.PlayClipAtPoint(coinSfx, transform.position);
+        scoreManager.Addscore(10);
+        Destroy(gameObject);
+        
     }
 
  
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * 100f * Time.deltaTime);
+        transform.Rotate(200f * Time.deltaTime * Vector3.forward);
     }
 }
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private int currentScore;
+    
+    void Start()
+    {
+        scoreText.text = "Score:0";
+    }
+
+    public void Addscore(int score)
+    {
+        currentScore += score;
+        scoreText.text = "Score: " + currentScore.ToString();
+    }
+}
